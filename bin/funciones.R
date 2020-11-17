d22598a5e112dccea30a1d688815a3aa17ec357b
necrosnake91 <rodolfo.peacewalker@gmail.com>
2020-11-16 22:57:46 -0600

    Added functions to get DE genes and volcano plots from DESeq2 results

--- bin/funciones.R
+++ bin/funciones.R
@@ -37,3 +37,36 @@ volcano_edgeR <- function(volc, lfc, padj) {
           legend.position = "bottom")
   return(volcano_plot)
 }
+
+DESEqResults <- function(dge.obj, logfc, FDR) {
+  ##This function adds a new column (T or F) according to the FDR and LFC of each gene in edgeR list of DEG
+  ##dge.obj: List with DEG
+  volc <- dge.obj %>%
+    mutate(condition = ifelse((dge.obj$log2FoldChange > logfc) & (dge.obj$padj < FDR), "Over-expressed",
+                              ifelse((dge.obj$log2FoldChange < -logfc) & (dge.obj$padj < FDR), "Sub-expressed",
+                                     ifelse((dge.obj$log2FoldChange > logfc) & (dge.obj$padj > FDR), "NS",
+                                            ifelse((dge.obj$log2FoldChange < -logfc) & (dge.obj$padj > FDR), "NS",
+                                                   ifelse((dge.obj$log2FoldChange < logfc) & (dge.obj$padj > FDR), "NS", "NS")))))) %>%
+    drop_na()
+  return(volc)
+}
+
+volcano_DESeq2 <- function(volc, lfc, FDR) {
+  ##The function returns the volcano plot of the dataset
+  ##volcano_data: Data frame containing the data to create the volcano plot
+  volcano_plot <- ggplot(volc)+
+    geom_point(aes(x = log2FoldChange, y = -log10(padj), color = condition))+
+    scale_color_manual(name = "Condition",
+                       labels = paste(c("NS", "Over-expressed", "Sub-expressed"), c(sum(volc$condition == "NS"), sum(volc$condition == "Over-expressed"), sum(volc$condition == "Sub-expressed"))),
+                       values = c("#6e6d6e","#d84b47","#66c343"))+
+    geom_vline(aes(xintercept = lfc), linetype = "dashed")+
+    geom_vline(aes(xintercept = -lfc), linetype = "dashed")+
+    geom_hline(aes(yintercept = -log10(FDR)), linetype = "dashed")+
+    theme_set(theme_bw())+
+    theme(plot.title = element_text(face = "bold", size = 18), 
+          axis.title = element_text(size = 18),
+          legend.title = element_text(face = "bold", size = 15),
+          legend.text = element_text(size = 15), 
+          legend.position = "bottom")
+  return(volcano_plot)
+}
